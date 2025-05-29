e0972488791a51fa086c4d78ced204ee7de1a528
Mateo Chumbita <mateo.chumbita@gmail.com>
2025-05-28 22:56:15 -0300

    feat: add support for additional frontend origin in CORS configuration

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ const models = initModels(sequelizeDB);
 
 // Middlewares
 app.use(cors({
-  origin: ["http://localhost:3000", "http://localhost:3001"],
+  origin: ["http://localhost:3000", "http://localhost:3001",  "http://localhost:5173" ],
   credentials: true,
 }));
 app.use(express.json()); // Middleware para procesar JSON
