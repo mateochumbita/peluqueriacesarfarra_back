3fc71a3f74e0a9e6b1186f48d4ecd053f02affe4
Mateo Chumbita <mateo.chumbita@gmail.com>
2025-06-01 12:04:42 -0300

    update hairdressers model

--- src/models/Hairdressers.js
+++ src/models/Hairdressers.js
@@ -1,60 +1,65 @@
-import { DataTypes } from 'sequelize';
+import { DataTypes } from "sequelize";
 
 const Model = (sequelize) => {
-  return sequelize.define('Hairdressers', {
-    Id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true
-    },
-     Dni: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    Nombre: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-   
-    Email:{
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    Telefono:{
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-   
-    IdUser: { // Clave foránea para relacionar con Users
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: 'Users', // Nombre de la tabla referenciada
-        key: 'Id'
-      }
-    }
-  }, {
-    sequelize,
-    tableName: 'Hairdressers',
-    schema: 'public',
-    timestamps: false,
-    indexes: [
-      {
-        name: "Hairdressers_pkey",
-        unique: true,
-        fields: [
-          { name: "Id" },
-        ]
+  return sequelize.define(
+    "Hairdressers",
+    {
+      Id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
       },
-      {
-        name: "Hairdressers_IdUser_idx", // Cambiado para evitar conflictos
-        fields: [
-          { name: "IdUser" },
-        ]
-      }
-    ]
-  });
+      Dni: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      Nombre: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+
+      Email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      Telefono: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      FechaRegistro: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+
+      IdUser: {
+        // Clave foránea para relacionar con Users
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: "Users", // Nombre de la tabla referenciada
+          key: "Id",
+        },
+      },
+    },
+    {
+      sequelize,
+      tableName: "Hairdressers",
+      schema: "public",
+      timestamps: false,
+      indexes: [
+        {
+          name: "Hairdressers_pkey",
+          unique: true,
+          fields: [{ name: "Id" }],
+        },
+        {
+          name: "Hairdressers_IdUser_idx", // Cambiado para evitar conflictos
+          fields: [{ name: "IdUser" }],
+        },
+      ],
+    }
+  );
 };
 
 export default Model;
