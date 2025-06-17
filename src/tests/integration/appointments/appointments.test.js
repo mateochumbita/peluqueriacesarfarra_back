6dfd9214f75a3e4132d4be6bb616f51660bfb142
Mateo Chumbita <mateo.chumbita@gmail.com>
2025-06-16 22:42:42 -0300

    Add cancel Appointment test

--- src/tests/integration/appointments/appointments.test.js
+++ src/tests/integration/appointments/appointments.test.js
@@ -41,3 +41,45 @@ describe("POST /api/v1/appointments", () => {
     expect(res.body.Id).toBeDefined();
   });
 });
+
+
+describe("PUT /api/v1/appointments/:id", () => {
+  let token;
+
+  beforeAll(async () => {
+    const loginRes = await request(app).post("/api/v1/auth/login").send({
+      username: "administrador",
+      password: "Admin2025*",
+    });
+
+    token = loginRes.body.token;
+  });
+
+  it("debería cancelar el turno", async () => {
+    // Primero creamos el turno
+    const turnoNuevo = {
+      Fecha: "2023-05-16",
+      Hora: "11:00:00",
+      IdHairdresser_Service: 11,
+      IdCliente: 1,
+      Estado: "Reservado",
+    };
+
+    const crearRes = await request(app)
+      .post("/api/v1/appointments")
+      .set("Authorization", `Bearer ${token}`)
+      .send(turnoNuevo);
+
+    expect(crearRes.statusCode).toBe(201);
+    const turnoId = crearRes.body.Id;
+
+    // Luego actualizamos el estado a "Cancelado"
+    const actualizarRes = await request(app)
+      .put(`/api/v1/appointments/${turnoId}`)
+      .set("Authorization", `Bearer ${token}`)
+      .send({ Estado: "Cancelado" });
+
+    expect(actualizarRes.statusCode).toBe(200);
+    expect(actualizarRes.body).toHaveProperty("Estado", "Cancelado");
+  });
+});
