6b57cfc7b5689716e72fab97d8220d17ddac2fa5
Mateo Chumbita <mateo.chumbita@gmail.com>
2025-07-14 16:49:30 -0300

    Add secure in emailService

--- src/service/emailService.js
+++ src/service/emailService.js
@@ -1,12 +1,13 @@
 // src/services/emailService.js
-import nodemailer from 'nodemailer';
-import dotenv from 'dotenv';
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
- host:'smtp.gmail.com',
-port: 465,
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS, // Recomendado: contraseña de aplicación
@@ -15,9 +16,9 @@ port: 465,
 
 transporter.verify((error, success) => {
   if (error) {
-    console.error('❌ Error al conectar con el servidor de correo:', error);
+    console.error("❌ Error al conectar con el servidor de correo:", error);
   } else {
-    console.log('✅ Servidor de correo listo');
+    console.log("✅ Servidor de correo listo");
   }
 });
 
