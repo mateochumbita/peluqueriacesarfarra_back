1628fada84ee6fdd64622593bd8bf254cef65cb4
mateochumbita <mateo.chumbita@gmail.com>
2026-03-09 21:28:59 -0300

    connection database

--- src/database/connection.database.js
+++ src/database/connection.database.js
@@ -1,25 +1,26 @@
-import { Sequelize } from 'sequelize';
-import 'dotenv/config';
-import pg from 'pg'; 
+import { Sequelize } from "sequelize";
+import "dotenv/config";
+import pg from "pg";
 
 // Configuración de la conexión a la base de datos
 export const sequelizeDB = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres',
-    dialectModule: pg, 
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false
-        },
+  dialect: "postgres",
+  dialectModule: pg,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
     },
+  },
 });
 
 // prueba de conexión a la base de datos
 (async () => {
-    try {
-        await sequelizeDB.authenticate();
-        console.log('DATABASE connected');
-    } catch (error) {
-        console.error('DATABASE connection failed:', error);
-    }
+  try {
+    await sequelizeDB.authenticate();
+    console.log("DATABASE connected");
+    console.log("🔍 DATABASE_URL RECIBIDA:", process.env.DATABASE_URL);
+  } catch (error) {
+    console.error("DATABASE connection failed:", error);
+  }
 })();
